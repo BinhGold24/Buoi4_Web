6901632ce9ae3f1a46093e56cdf1900c3ba567f2
BinhGold24 <nb241006789@gmail.com>
2026-06-01 22:11:03 +0700

    Add project files.

---
+++ Bai 6/Controllers/BookController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Bai6_Validation.Models;
+
+namespace Bai6_Validation.Controllers
+{
+    public class BookController : Controller
+    {
+        private static List<Book> books = new List<Book>();
+
+        public IActionResult Index()
+        {
+            return View(books);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Book book)
+        {
+            if (ModelState.IsValid)
+            {
+                book.Id = books.Count + 1;
+
+                books.Add(book);
+
+                return RedirectToAction("Index");
+            }
+
+            return View(book);
+        }
+    }
+}
---
+++ Bai 6/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Bai6_Validation.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Không được để trống")]
+        public string Name { get; set; }
+
+        [Range(0.01, double.MaxValue,
+            ErrorMessage = "Giá phải lớn hơn 0")]
+        public double Price { get; set; }
+    }
+}
